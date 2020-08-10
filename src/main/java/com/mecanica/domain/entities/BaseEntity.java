94b18562e95fa18f1859c530b29403ad3d4a55b4
wesley xavier <wesleyxavier_2@hotmail.com>
2020-08-10 17:03:08 -0300

    serialize dateTime

--- src/main/java/com/mecanica/domain/entities/BaseEntity.java
+++ src/main/java/com/mecanica/domain/entities/BaseEntity.java
@@ -8,6 +8,11 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+
 import org.hibernate.annotations.GenericGenerator;
 
 import lombok.Getter;
@@ -24,5 +29,7 @@ public abstract class BaseEntity {
     private UUID id;
 
     @Column(nullable = false)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
     private LocalDateTime dataCadastro;
 }
